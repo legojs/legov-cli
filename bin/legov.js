367a9e75d86309227f79545e7a1c7d104a027e89
legojs <legojs@qq.com>
2015-12-16 20:03:53 +0800

    新增 start 命令 启动服务。

--- bin/legov.js
+++ bin/legov.js
@@ -3,8 +3,12 @@ var program = require('commander');
 var app_info = require('../package.json');
 var chalk = require('chalk');
 var init = require('../lib/init');
-var sown = require('../lib/generator');
-var target = process.cwd();
+var generator = require('../lib/generator');
+var update = require('../lib/update');
+var start = require('../lib/start');
+var proxy = require('../lib/proxy');
+var add = require('../lib/add');
+var current = process.cwd();
 
 //基本信息
 program
@@ -16,8 +20,8 @@ program
   .command('init')
   .description('初始化前端工程。')
   .action(function () {
-    console.log('\n  开始初始化：' + chalk.green(target));
-    init(target);
+    console.log('\n  开始初始化：' + chalk.green(current)) + '\n';
+    init(current);
   }).on('--help', function() {
     console.log('  Examples:');
     console.log('    $ mkdir <工程目录>');
@@ -25,22 +29,78 @@ program
     console.log('    $ %s %s', chalk.magenta('lv'), chalk.cyan('init'));
   });
 
-//$ lv seed
+//$ lv generator <seed>
 program
   .command('generator <seed>')
   .alias('g')
-  .description('播种项目（请确定种子已经就位）。')
+  .description('播种站点（请确定种子已经就位）。')
   .action(function(seed){
-    console.log('\n  [%s]开始播种项目~~', chalk.green(seed));
-    sown(target, seed);
+    console.log('\n  [%s]开始播种站点~~', chalk.green(seed)) + '\n';
+    generator(current, seed);
   }).on('--help', function() {
     console.log('  Examples:');
-    console.log('    $ cd "工程目录/@legov"');
+    console.log('    $ cd "<工程目录>/@legov"');
     console.log('    $ tnpm i @tencent/wxpay-seed-codeigniter');
-    console.log('    $ cd ..              #返回工程目录');
-    console.log('    $ mkdir <项目目录>   #建立项目目录（或者从 Svn or Git 拉取）');
-    console.log('    $ cd <项目目录>');
-    console.log('    $ %s %s codeigniter', chalk.magenta('lv'), chalk.cyan('g'));
+    console.log('    $ cd ..              #返回<工程目录>');
+    console.log('    $ mkdir <站点目录>   #建立<站点目录>（或者从 Svn or Git 拉取）');
+    console.log('    $ cd <站点目录>');
+    console.log('    $ %s %s @tencent/wxpay-seed-codeigniter', chalk.magenta('lv'), chalk.cyan('g'));
   });
+
+//$ lv update <seed>
+program
+  .command('update <seed>')
+  .alias('up')
+  .description('升级站点（请确定种子已经就位）。')
+  .action(function(seed){
+    console.log('\n  [%s]开始升级站点~~', chalk.green(seed)) + '\n';
+    update(current, seed);
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log('    $ cd "@legov"');
+    console.log('    $ tnpm up @tencent/wxpay-seed-codeigniter');
+    console.log('    $ %s %s @tencent/wxpay-seed-codeigniter', chalk.magenta('lv'), chalk.cyan('u'));
+  });
+
+//$ lv start
+program
+  .command('start')
+  .alias('s')
+  .description('启动站点。')
+  .action(function(){
+    start(current);
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log('    $ cd wxpay.oa.com-boss');
+    console.log('    $ %s %s', chalk.magenta('lv'), chalk.cyan('s'));
+  });
+
+//$ lv proxy
+program
+  .command('proxy')
+  .alias('p')
+  .description('启动代理服务器。')
+  .action(function(){
+    proxy(current);
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log('    $ cd wxpay.oa.com-boss');
+    console.log('    $ %s %s', chalk.magenta('lv'), chalk.cyan('p'));
+  });
+
+//$ lv add <leaf>
+program
+  .command('add <leaf>')
+  .description('增加项目（请确定叶子已经就位）。')
+  .action(function(leaf){
+    console.log('\n  [%s]开始增加项目~~', chalk.green(leaf)) + '\n';
+    add(current, leaf);
+  }).on('--help', function() {
+    console.log('  Examples:');
+    console.log('    $ cd "<工程目录>/@legov"');
+    console.log('    $ tnpm i @tencent/wxpay-leaf-flame');
+    console.log('    $ cd ../<站点目录>');
+    console.log('    $ %s %s flame -n project', chalk.magenta('lv'), chalk.cyan('add'));
+  });  
 
 program.parse(process.argv);
